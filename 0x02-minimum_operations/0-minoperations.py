c50cec53c1731515c3ceb751dfe6a4b5da50397b
Forster Asamany <asamanyforster@yahoo.com>
2023-12-14 15:35:54 -0500

    Update Minimum operations

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 """
-Write a method to determine the minimum operations
-needed to achieve exactly n H characters in a file,
-given n as input.
+Write a method to determine the minimum
+operations needed to achieve exactly
+n H characters in a file,given n as input.
 """
 
 
@@ -13,13 +13,13 @@ def minOperations(n):
     """
     result = 0
     divisor = 2
-
+    
     while n > 1:
         if n % divisor == 0:
             result += divisor
             n //= divisor
         else:
             divisor += 1
-
+            
     return result
 
