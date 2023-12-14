47325918736df2fc314c332ec012f20dd0c6c015
Forster Asamany <asamanyforster@yahoo.com>
2023-12-14 15:51:40 -0500

    Update Minimum operations

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -12,14 +12,11 @@ def minOperations(n):
     If n is impossible to achieve, return 0
     """
     result = 0
-    divisor = 2
-    
+    x = 2
     while n > 1:
-        if n % divisor == 0:
-            result += divisor
-            n //= divisor
-        else:
-            divisor += 1
-            
-    return result
+        while n % x == 0:
+            result += x
+            n /= x
+        x += 1
 
+    return result
